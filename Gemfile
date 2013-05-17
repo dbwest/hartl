4a7182bd7908ab80e5b81cd8a49b7079272ad181
David West <david.b.west@gmail.com>
2013-05-17 16:07:03 -0400

    finish ;ayout

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
+gem 'bootstrap-sass', '2.1'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -24,6 +25,7 @@ group :assets do
   # gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
+  gem 'modernizr-rails'
 end
 
 gem 'jquery-rails'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,23 +4,23 @@ describe "StaticPages" do
 
 	describe "Home Page" do
 		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
+			visit home_path
 			page.should have_content('Sample App')
 		end
 	end
 
 	describe "Help Page" do
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_content('Help')
 		end
 	end
 
 	describe "About Page" do
 		it "should have the content 'About'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_content('About')
 		end
-	end
+	end 
 
 end
