94a24304dc9fcced51c92a8be835f5b891c7ec0a
David West <david.b.west@gmail.com>
2013-05-21 18:16:26 -0400

    posts feeds etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts
   end
 
   def edit
@@ -47,13 +48,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,5 +2,24 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	subject {page}
+
+	describe "Home page" do
+		describe "for signed in" do
+			let(:user) {FactoryGirl.create(:user)}
+			before do
+				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+				FactoryGirl.create(:micropost, user: user, content: "asdfas")
+				sign_in user
+				visit root_path
+			end
+
+			it "should render the user's feed" do
+				user.feed.each do |item|
+					page.should have_selector("li##{item.id}", text: item.content)
+				end
+			end
+		end
+	end
 
 end
